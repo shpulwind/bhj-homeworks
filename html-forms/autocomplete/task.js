2630b981aa3e05c5f01ed7f4c73d00c6aae873ff
shpulwind <shpulvind@ya.ru>
2019-11-24 14:56:04 +0300

    Task 3 homework "HTML Forms" complited

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,13 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    let array = [];
+    for(let element of this.input.options) {
+      if(element.textContent.indexOf(this.searchInput.value) >= 0) {
+        array.push({text: element.textContent, value: element.value})
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    }
+    return array;
   }
 }
 
